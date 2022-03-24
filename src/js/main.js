0cd782c1305b72474c443cc79e50d28c1506815f
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-03-24 18:46:52 -0300

    Criando ambiente e passando o design para html e css

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+/*======== Deixando CONTAINERS com TAMANHO DA TELA =======*/
+let cabecalho = document.querySelector('.cabecalho')
+cabecalho.style.height = window.innerHeight + 'px'
+
+let atuacoes = document.querySelector('.atuacoes')
+atuacoes.style.height = window.innerHeight + 'px'
