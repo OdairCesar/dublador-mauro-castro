186aad487f57a4f3c2a5bc54216bab0d0a12dbbd
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-03-25 16:02:46 -0300

    adicionando funcionalidades

---
+++ src/js/db-dublagens.js
@@ -0,0 +1,263 @@
+dublagens = [
+    /*{
+        id: 1,
+        personagem: 'Brutus',
+        movie: 'O Marinheiro Popeye 3D',
+        image: './src/img/1.png'
+    }*/{
+        id: 1, 
+        personagem: 'Yama',
+        movie: 'Operação Big Hero',
+        image: './src/img/51.png'
+    }, {
+        id: 2,
+        personagem: 'Don Kanonji',
+        movie: 'Bleach',
+        image: './src/img/2.png'
+    }, {
+        id: 3,
+        personagem: 'Lord',
+        movie: 'Dragon Eyes',
+        image: './src/img/3.png'
+    }, {
+        id: 4,
+        personagem: 'Presidente dos EUA',
+        movie: '24h O Filme: A Redenção',
+        image: './src/img/4.png'
+    }, {
+        id: 5,
+        personagem: 'Harley',
+        movie: 'O Vigilante',
+        image: './src/img/5.png'
+    }, {
+        id: 6,
+        personagem: 'Algethi de Hércules',
+        movie: 'CDZ: A Saga de Hades',
+        image: './src/img/6.png'
+    }, {
+        id: 7,
+        personagem: 'Max Payne',
+        movie: 'Max Payne 1',
+        image: './src/img/7.png'
+    }, {
+        id: 8,
+        personagem: 'Jefferson Smith',
+        movie: 'Max Steel: O Filme',
+        image: './src/img/8.png'
+    }, {
+        id: 9,
+        personagem: 'Professor Trask',
+        movie: 'Wolverine e os X Men',
+        image: './src/img/9.png'
+    }, {
+        id: 10,
+        personagem: 'Rei Cutelo',
+        movie: 'Dragon Ball',
+        image: './src/img/10.png'
+    }, {
+        id: 11,
+        personagem: 'Rei Vegeta',
+        movie: 'Dragon Ball',
+        image: './src/img/11.png'
+    }, {
+        id: 12,
+        personagem: 'Zeb',
+        movie: 'Star Wars Rebell',
+        image: './src/img/12.png'
+    }, {
+        id: 13,
+        personagem: 'Helios',
+        movie: 'Bakugan',
+        image: './src/img/13.png'
+    }, {
+        id: 14,
+        personagem: 'Taylor',
+        movie: 'Dinossauro Rei',
+        image: './src/img/14.png'
+    }, {
+        id: 15,
+        personagem: 'Gormiti',
+        movie: 'Obscurio',
+        image: './src/img/15.png'
+    }, {
+        id: 16,
+        personagem: 'Jack Jones',
+        movie: 'Comando de Elite',
+        image: './src/img/16.png'
+    }, {
+        id: 17,
+        personagem: 'Doc',
+        movie: 'Amigos Inseparáveis',
+        image: './src/img/17.png'
+    }, {
+        id: 18,
+        personagem: 'Abraham Ford',
+        movie: 'The Walking Dead',
+        image: './src/img/18.png'
+    }, {
+        id: 19,
+        personagem: 'Dr. Elmer Hartman',
+        movie: 'Uma Família da Pesada',
+        image: './src/img/19.png'
+    }, {
+        id: 20,
+        personagem: 'Diretor Lewis',
+        movie: 'American Dead',
+        image: './src/img/20.png'
+    }, {
+        id: 21,
+        personagem: 'K2',
+        movie: 'B13: 13º Distrito',
+        image: './src/img/21.png'
+    }, {
+        id: 22,
+        personagem: 'Hulk',
+        movie: 'Hulk e os Agentes de Smash',
+        image: './src/img/22.png'
+    }, {
+        id: 23,
+        personagem: 'Joe',
+        movie: 'Debi e Lóide: Dois Idiotas em Apuros',
+        image: './src/img/23.png'
+    }, {
+        id: 24,
+        personagem: 'Sergius',
+        movie: 'A Papisa Joana',
+        image: './src/img/24.png'
+    }, {
+        id: 25,
+        personagem: 'Sansão',
+        movie: 'A Bíblia',
+        image: './src/img/25.png'
+    }, {
+        id: 26,
+        personagem: 'Jito Hiroshi',
+        movie: 'Super Campeões',
+        image: './src/img/26.png'
+    }, {
+        id: 27,
+        personagem: 'Jito Hiroshi',
+        movie: 'Locked Down',
+        image: './src/img/27.png'
+    }, {
+        id: 28,
+        personagem: 'Mr. Wellington',
+        movie: 'O Garoto do Alabama',
+        image: './src/img/28.png'
+    }, {
+        id: 29,
+        personagem: 'Capitão Augustus Black',
+        movie: 'As Aventuras de Jackie Chan',
+        image: './src/img/29.png'
+    }, {
+        id: 30,
+        personagem: 'Tahm Kench',
+        movie: 'Legend of Legends',
+        image: './src/img/30.png'
+    }, {
+        id: 31,
+        personagem: 'John Madec',
+        movie: 'Fora de Alcance',
+        image: './src/img/31.png'
+    }, {
+        id: 32,
+        personagem: 'Giran',
+        movie: 'Dragon Ball',
+        image: './src/img/32.png'
+    }, {
+        id: 33,
+        personagem: 'Jack',
+        movie: 'Sete Almas',
+        image: './src/img/33.png'
+    }, {
+        id: 34,
+        personagem: 'Umbopa',
+        movie: 'As Minas do Rei Salomão',
+        image: './src/img/34.png'
+    }, {
+        id: 35,
+        personagem: 'Protoss Zeratul',
+        movie: 'Starcraft II',
+        image: './src/img/35.png'
+    }, {
+        id: 36,
+        personagem: 'Brok',
+        movie: 'God of War',
+        image: './src/img/36.png'
+    }, {
+        id: 37,
+        personagem: 'Rais',
+        movie: 'Dying Light',
+        image: './src/img/37.png'
+    }, {
+        id: 38,
+        personagem: 'Reza Nouri',
+        movie: 'Slinter Cell: BlackList',
+        image: './src/img/38.png'
+    }, {
+        id: 39,
+        personagem: 'Prefeito Leãonardo',
+        movie: 'Zootopia',
+        image: './src/img/39.png'
+    }, {
+        id: 40,
+        personagem: 'Camús de Aquário',
+        movie: 'CDZ: A Lenda do Santuário',
+        image: './src/img/40.png'
+    }, {
+        id: 41,
+        personagem: 'Presidente dos EUA',
+        movie: 'Invasão a Casa Branca',
+        image: './src/img/41.png'
+    }, {
+        id: 42,
+        personagem: 'Tony Beets',
+        movie: 'Febre do Ouro: A Decisão',
+        image: './src/img/42.png'
+    }, {
+        id: 43,
+        personagem: 'Briar',
+        movie: 'Bonnie Bears',
+        image: './src/img/43.png'
+    }, {
+        id: 44,
+        personagem: 'Odin',
+        movie: 'Smite',
+        image: './src/img/44.png'
+    }, {
+        id: 45,
+        personagem: 'Keneth',
+        movie: 'Duplex',
+        image: './src/img/45.png'
+    }, {
+        id: 46,
+        personagem: 'Franklin',
+        movie: 'Hunter x Hunter',
+        image: './src/img/46.png'
+    }, {
+        id: 47,
+        personagem: 'Denard',
+        movie: 'Grace de Mônaco',
+        image: './src/img/47.png'
+    }, {
+        id: 48,
+        personagem: 'Frank Nitti',
+        movie: 'Inimigos Publicos',
+        image: './src/img/48.png'
+    }, {
+        id: 49, 
+        personagem: 'Uglúk',
+        movie: 'Senhor dos Anéis: Segunda Torre',
+        image: './src/img/49.png'
+    }, {
+        id: 50, 
+        personagem: 'Heathcliff',
+        movie: 'Operação Big Hero',
+        image: './src/img/50.png'
+    }/*, {
+        id: 51, 
+        personagem: 'Yama',
+        movie: 'Operação Big Hero',
+        image: './src/img/51.png'
+    }*/
+]
--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,81 @@ cabecalho.style.height = window.innerHeight + 'px'
 
 let atuacoes = document.querySelector('.atuacoes')
 atuacoes.style.height = window.innerHeight + 'px'
+
+/*================== Adionando os cards no HTML ====================*/
+const listDublagens = document.getElementById('list-dublagens')
+let lista
+let i = 0
+let cont = 15
+
+function carregarImagens(isReloading) {
+  if (cont < 60) {//Na pasta img tem somente 60 cards, ou seja, não é possivel ter mais de 60 cards na listDublagens
+    isReloading ? cont += 15 : cont = 15//Essa função é usada no iniciar da pagina e depois no click de um botão, por isso usamos o 'cont' como limitador do FOR
+
+    for (i; i < cont; i++) {
+      lista = '<div class="card-dublagem" id="' + dublagens[i].id + '">'
+        + '<img src="' + dublagens[i].image + '" alt="' + dublagens[i].personagem + '">'
+        + '<div class="info-card-dublagem">'
+        + '<h4>' + dublagens[i].personagem + '</h4>'
+        + '<p>' + dublagens[i].movie + '</p>'
+        + '</div>'
+        + '</div>'
+      listDublagens.innerHTML += lista
+    }
+  }
+}
+carregarImagens(false)
+
+/*================== Adionando mais cards ao clicar no button ====================*/
+const button = document.getElementById('button-dublagens')
+button.addEventListener('click', e => carregarImagens(true))
+
+/*================== Mudando o video da seção ATUAÇÕES ====================*/
+const buttonNext = document.getElementById('button-next-atuacoes')
+const buttonPrev = document.getElementById('button-prev-atuacoes')
+let contVideo = 1 //Numeração do video que irá aparecer
+
+function criarVideoNovo(video) {//Função criar Video Novo
+  const novoVideo = document.createElement('video')
+  const source = document.createElement('source')
+
+  source.setAttribute('src', video + '.mp4')
+  source.setAttribute('type', 'video/mp4')
+  novoVideo.setAttribute('id', 'video-atuacoes')
+  novoVideo.setAttribute('class', 'video-atuacoes')
+  novoVideo.setAttribute('controls', 'true')
+  novoVideo.setAttribute('poster', video + '.png')
+  novoVideo.appendChild(source)
+
+  return novoVideo
+}
+
+buttonNext.addEventListener('click', function () {
+  if (contVideo < 4) {//Como são 4 videos so podemos fazer até o video 04
+    contVideo++
+    //Deletando video atual
+    document.getElementById('video-atuacoes').remove()
+
+    //Adicionando video novo
+    const divAtuacoes = document.getElementById('videos-ator')
+    const novoVideo = criarVideoNovo('./src/video/0' + contVideo)
+    divAtuacoes.appendChild(novoVideo)
+  }
+})
+
+buttonPrev.addEventListener('click', function () {
+  if (contVideo > 1) {//Como a numeração começa em 01 so podemos voltar até o video 01
+    contVideo--
+
+    //Deletando video atual
+    document.getElementById('video-atuacoes').remove()
+
+    //Adicionando video novo
+    const divAtuacoes = document.getElementById('videos-ator')
+    const novoVideo = criarVideoNovo('./src/video/0' + contVideo)
+    divAtuacoes.appendChild(novoVideo)
+  }
+  console.log(contVideo)
+})
+
+
